34addc26d922e275f239f1dd17f19d13a85f06b1
Tomek <heflik@gmail.com>
2017-11-20 17:52:04 +0100

    Formated output

--- tacx-usb.py
+++ tacx-usb.py
@@ -1,22 +1,32 @@
-import usb.core, time, os
+import usb.core, time, sys, signal
 import usb.util
 import trainer
-while True:
-    dev = trainer.get_trainer()
-    if not dev:
-        print "Waiting for Tacx next 5s."
-        time.sleep(5)
-    else:
-        print "Found initializing"
-        trainer.initialise_trainer(dev)
-        break
+
+def nice_exit():
+    print "\nBye!!!\n"
+    sys.exit(0)
 
 while True:
-    trainer.send(dev, 0, 0)
-    time.sleep(0.3)
-    speed, pedecho, heart_rate, force_index, cadence = trainer.receive(dev)
-    if speed is None:
-        print "No output"
-        break
-    else:
-        print "SPEED: " + str(speed) + "\tPECHO: " + str(pedecho) + "\tHR: " + str(heart_rate) + "\tFORCE: " + str(force_index) + "\tCADENCE: " + str(cadence)
+    try:
+        dev = trainer.get_trainer()
+        if not dev:
+            print "Waiting for Tacx next 5s."
+            time.sleep(5)
+        else:
+            print "Found initializing"
+            trainer.initialise_trainer(dev)
+            break
+    except KeyboardInterrupt:
+        nice_exit()
+while True:
+    try:
+        trainer.send(dev, 3, 0)
+        time.sleep(0.3)
+        speed, pedecho, heart_rate, force_index, cadence = trainer.receive(dev)
+        if speed is None:
+            print "No output"
+            break
+        else:
+            print "SPEED: " + str(speed) + "\tPECHO: " + str(pedecho) + "\tHR: " + str(heart_rate) + "\tFORCE: " + str(force_index) + "\tCADENCE: " + str(cadence)
+    except KeyboardInterrupt:
+        nice_exit()
